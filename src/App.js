81e2fabf883a74e02229bb1a1d9dc69f931c73a3
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-09-02 19:23:58 +0200

    Add Spinner

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Spinner from './components/ui/spinner/Spinner';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Spinner />
+    </div>
+  );
 }
 
 export default App;
