89ccb97d8e359cb371a73e02a75bce3dfb588991
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-09-10 22:56:58 +0200

    Initialize routes

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-import express from 'express';
+const express = require('express');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.json({ response: 'ok' });
 });
+
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.port || 5000;
 
