3edb00dd44c0ef342865f634dcd59c3e017d2b04
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-09-11 20:20:22 +0200

    Prepare for frontapp deployment

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const connectDB = require('./config/db');
+const path = require('path');
 const app = express();
 
 connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.json({ response: 'ok' });
-});
-
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 // app.use('/api/settings', require('./routes/settings'));
 // app.use('/api/workout', require('./routes/workout'));
 // app.use('/api/day', require('./routes/day'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontapp/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontapp', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
