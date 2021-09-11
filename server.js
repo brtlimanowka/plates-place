a56d3ffeb1d2cc271dd935e105061748ffc7d23b
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-09-11 17:07:21 +0200

    Correct environmental variable

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.get('/', (req, res) => {
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
