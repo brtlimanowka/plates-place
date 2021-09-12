7c2dc759fdf02d697e362b3259f54da0237838a8
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-09-12 12:21:35 +0200

    Add signup form

--- frontapp/src/components/pages/LandingPage.js
+++ frontapp/src/components/pages/LandingPage.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import CenteredCard from '../ui/templates/CenteredCard';
-// import SignupForm from './SignupForm';
-import frogImage from '../../assets/1606476323206.gif';
+import SignupForm from './SignupForm';
 import classes from './css/LandingPage.module.css';
 
 const LandingPage = () => {
   return (
     <CenteredCard height={'100vh'}>
       <main className={classes.card}>
-        <img src={frogImage} alt='żabe' />
         <h1>Welcome to Plates' Place</h1>
         <h3>An online PPL program planner</h3>
-        {/* <SignupForm /> */}
+        <SignupForm />
       </main>
     </CenteredCard>
   );
--- frontapp/src/components/pages/SignupForm.js
+++ frontapp/src/components/pages/SignupForm.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classes from './css/SignupForm.module.css';
 
 const SignupForm = () => {
+  const [isLogin, setIsLogin] = useState(false);
+  const switchModeHandler = () => {
+    setIsLogin(!isLogin);
+  };
   const submitHandler = (event) => {
     event.preventDefault();
   };
   return (
-    <form onSubmit={submitHandler}>
-      <h3>Sign up</h3>
-      <label htmlFor='email'>Email:</label>
-      <input type='email' id='email' />
-      <label htmlFor='password'>Password:</label>
-      <input type='password' id='password' />
-      <button>Register</button>
-    </form>
+    <div className={classes['form-container']}>
+      <form onSubmit={submitHandler}>
+        <h2>{isLogin ? 'Login' : 'Sign up'}</h2>
+        <div className={classes['form-control']}>
+          <label htmlFor='email'>Email</label>
+          <input type='email' id='email' />
+        </div>
+        <div className={classes['form-control']}>
+          <label htmlFor='password'>Password</label>
+          <input type='password' id='password' />
+        </div>
+        <div className={classes['form-actions']}>
+          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          <button type='button' onClick={switchModeHandler}>
+            {isLogin ? 'Create new account' : 'Login with existing account'}
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
