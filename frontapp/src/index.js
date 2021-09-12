1e47dccc4754377cf6b9597a6d00ada625aa96ac
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-09-12 13:09:03 +0200

    Create auth Context

--- frontapp/src/index.js
+++ frontapp/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AuthContext from './context/auth/AuthState';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <AuthContext>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </AuthContext>,
   document.getElementById('root')
 );
