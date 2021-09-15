bf74dcf89da56684cac8d540270a17b9c2eab33c
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-09-15 14:34:17 +0200

    Pass height as a prop to Spinner

--- frontapp/src/components/ui/elements/Spinner.js
+++ frontapp/src/components/ui/elements/Spinner.js
@@ -2,9 +2,9 @@ import React from 'react';
 import CenteredCard from '../templates/CenteredCard';
 import classes from './css/Spinner.module.css';
 
-const Spinner = () => {
+const Spinner = (props) => {
   return (
-    <CenteredCard height={'100vh'}>
+    <CenteredCard height={props.height}>
       <div id={classes.spinner_dot1} className={classes.spinner_dot}></div>
       <div id={classes.spinner_dot2} className={classes.spinner_dot}></div>
       <div id={classes.spinner_dot3} className={classes.spinner_dot}></div>
