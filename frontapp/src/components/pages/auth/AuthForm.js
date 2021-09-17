3e37223b1d824e7a0a06f21f66abcf6a94703a07
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-09-17 21:22:22 +0200

    Split inputs into components

--- frontapp/src/components/pages/auth/AuthForm.js
+++ frontapp/src/components/pages/auth/AuthForm.js
@@ -1,19 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import AuthInput from './AuthInput';
 import Spinner from '../../ui/elements/Spinner';
 import classes from '../css/Authentication.module.css';
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    name: null,
+    email: null,
+    password: null,
+  });
   const [isFormValid, setIsFormValid] = useState(false);
+
+  useEffect(() => {
+    if (Object.values(formData).every((value) => value !== null)) {
+      setIsFormValid(true);
+    }
+  }, [formData]);
 
   const switchModeHandler = () => {
     setIsLogin((prevIsLogin) => !prevIsLogin);
   };
 
+  const nameConfirmed = (name) => {
+    setFormData((prevFormData) => {
+      return { ...prevFormData, name };
+    });
+  };
+  const emailConfirmed = (email) => {
+    setFormData((prevFormData) => {
+      return { ...prevFormData, email };
+    });
+  };
+  const passwordConfirmed = (password) => {
+    setFormData((prevFormData) => {
+      return { ...prevFormData, password };
+    });
+  };
+
   const submitHandler = (event) => {
     event.preventDefault();
     setIsLoading(true);
+    fetch(isLogin ? '/api/auth/' : '/api/userss/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
   };
 
   return (
@@ -23,6 +61,26 @@ const AuthForm = () => {
       ) : (
         <form onSubmit={submitHandler}>
           <h2 className={classes.header}>{isLogin ? 'Login' : 'Sign up'}</h2>
+          {!isLogin && (
+            <AuthInput
+              inputType='text'
+              inputFor='name'
+              inputLabel='Name'
+              confirmValue={nameConfirmed}
+            />
+          )}
+          <AuthInput
+            inputType='email'
+            inputFor='email'
+            inputLabel='Email'
+            confirmValue={emailConfirmed}
+          />
+          <AuthInput
+            inputType='password'
+            inputFor='password'
+            inputLabel='Password'
+            confirmValue={passwordConfirmed}
+          />
           <div className={classes['form-actions']}>
             <button
               disabled={!isFormValid}
--- frontapp/src/components/pages/auth/AuthInput.js
+++ frontapp/src/components/pages/auth/AuthInput.js
@@ -1,8 +1,114 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import classes from '../css/Authentication.module.css';
 
-const AuthInput = () => {
-  return <div></div>;
+const AuthInput = (props) => {
+  const [isTouched, setIsTouched] = useState(false);
+  const [inputHasError, setInputHasError] = useState(false);
+  const [inputError, setInputError] = useState(null);
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    if (isTouched && !inputHasError) {
+      props.confirmValue(inputValue);
+    } else {
+      props.confirmValue(null);
+    }
+    // eslint-disable-next-line
+  }, [isTouched, inputHasError, inputValue]);
+
+  const checkEmailAvailability = (email) => {
+    let encodedEmail = btoa(email);
+    setTimeout(() => {
+      fetch(`/api/users/${encodedEmail}`)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.message === 'OK') {
+            setInputHasError(false);
+            setInputError(null);
+          } else {
+            setInputHasError(true);
+            setInputError('Email already in use');
+          }
+        });
+    }, 500);
+  };
+
+  const validateInput = (value) => {
+    switch (props.inputFor) {
+      case 'name':
+        if (!!value.trim().length) {
+          setInputHasError(false);
+          setInputError(null);
+        } else {
+          setInputHasError(true);
+          setInputError('Required');
+        }
+        return;
+      case 'email':
+        if (!!value.trim().length && value.trim().includes('@')) {
+          setInputHasError(false);
+          setInputError(null);
+        } else {
+          setInputHasError(true);
+          setInputError('Required');
+        }
+        return;
+      case 'password':
+        if (value.trim().length >= 6) {
+          setInputHasError(false);
+          setInputError(null);
+        } else {
+          setInputHasError(true);
+          setInputError('At least 6 characters long');
+        }
+        return;
+      default:
+        break;
+    }
+  };
+
+  const inputFocusHandler = () => {
+    // clearTimeout(checkEmailAvailability);
+  };
+  const inputChangeHandler = (event) => {
+    setIsTouched(true);
+    setInputValue(event.target.value);
+    validateInput(event.target.value);
+    if (
+      props.inputFor === 'email' &&
+      !inputHasError &&
+      event.target.value !== ''
+    ) {
+      checkEmailAvailability(event.target.value);
+    }
+  };
+  const inputBlurHandler = (event) => {
+    validateInput(event.target.value);
+    if (
+      props.inputFor === 'email' &&
+      !inputHasError &&
+      event.target.value !== ''
+    ) {
+      checkEmailAvailability(event.target.value);
+    }
+  };
+
+  return (
+    <div className={classes['form-group']}>
+      <div className={classes['form-label']}>
+        <label htmlFor={props.inputFor}>{props.inputLabel}</label>
+        {inputHasError && <span>{inputError}</span>}
+      </div>
+      <input
+        type={props.inputType}
+        id={props.inputFor}
+        value={inputValue}
+        onFocus={inputFocusHandler}
+        onChange={inputChangeHandler}
+        onBlur={inputBlurHandler}
+      />
+    </div>
+  );
 };
 
 export default AuthInput;
