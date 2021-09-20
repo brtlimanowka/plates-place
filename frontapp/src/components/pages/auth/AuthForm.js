7cdb2e76d2dd14d084645eafc866cd53ed3b4346
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-09-20 19:21:01 +0200

    Persisting session

--- frontapp/src/components/pages/auth/AuthForm.js
+++ frontapp/src/components/pages/auth/AuthForm.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import AuthInput from './AuthInput';
 import Spinner from '../../ui/elements/Spinner';
 import classes from '../css/Authentication.module.css';
 
 const AuthForm = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isLogin, setIsLogin] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [formData, setFormData] = useState({
@@ -14,6 +15,13 @@ const AuthForm = () => {
   const [isFormValid, setIsFormValid] = useState(false);
   const [applicationError, setApplicationError] = useState(null);
 
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, [isLogin, isLoading]);
   useEffect(() => {
     if (isLogin) {
       setIsFormValid(formData.email && formData.password);
@@ -59,7 +67,10 @@ const AuthForm = () => {
           throw new Error(response.statusText);
         }
       })
-      .then((data) => console.log(data))
+      .then((data) => {
+        localStorage.setItem('token', data.token);
+        console.log(data);
+      })
       .catch((error) => {
         setApplicationError(error.message);
         console.error(error);
@@ -68,52 +79,61 @@ const AuthForm = () => {
   };
 
   return (
-    <div className={classes['form-container']}>
-      {isLoading && !applicationError && <Spinner height='300px' />}
-      {!isLoading && !!applicationError && (
-        <p className={classes['form-error']}>
-          Application error: {applicationError}
-        </p>
-      )}
-      {!isLoading && !applicationError && (
-        <form onSubmit={submitHandler}>
-          <h2 className={classes.header}>{isLogin ? 'Login' : 'Sign up'}</h2>
-          {!isLogin && (
-            <AuthInput
-              isLogin={isLogin}
-              inputType='text'
-              inputFor='name'
-              inputLabel='Name'
-              confirmValue={nameConfirmed}
-            />
+    <Fragment>
+      {isLoggedIn && <p>Logged in</p>}
+      {!isLoggedIn && (
+        <div className={classes['form-container']}>
+          {isLoading && !applicationError && <Spinner height='300px' />}
+          {!isLoading && !!applicationError && (
+            <p className={classes['form-error']}>
+              Application error: {applicationError}
+            </p>
           )}
-          <AuthInput
-            isLogin={isLogin}
-            inputType='email'
-            inputFor='email'
-            inputLabel='Email'
-            confirmValue={emailConfirmed}
-          />
-          <AuthInput
-            isLogin={isLogin}
-            inputType='password'
-            inputFor='password'
-            inputLabel='Password'
-            confirmValue={passwordConfirmed}
-          />
-          <div className={classes['form-actions']}>
-            <button
-              disabled={!isFormValid}
-              className={isFormValid ? '' : classes['form-invalid']}>
-              {isLogin ? 'Login' : 'Create Account'}
-            </button>
-            <button type='button' onClick={switchModeHandler}>
-              {isLogin ? 'Create new account' : 'Login with existing account'}
-            </button>
-          </div>
-        </form>
+          {!isLoading && !applicationError && (
+            <form onSubmit={submitHandler}>
+              <h2 className={classes.header}>
+                {isLogin ? 'Login' : 'Sign up'}
+              </h2>
+              {!isLogin && (
+                <AuthInput
+                  isLogin={isLogin}
+                  inputType='text'
+                  inputFor='name'
+                  inputLabel='Name'
+                  confirmValue={nameConfirmed}
+                />
+              )}
+              <AuthInput
+                isLogin={isLogin}
+                inputType='email'
+                inputFor='email'
+                inputLabel='Email'
+                confirmValue={emailConfirmed}
+              />
+              <AuthInput
+                isLogin={isLogin}
+                inputType='password'
+                inputFor='password'
+                inputLabel='Password'
+                confirmValue={passwordConfirmed}
+              />
+              <div className={classes['form-actions']}>
+                <button
+                  disabled={!isFormValid}
+                  className={isFormValid ? '' : classes['form-invalid']}>
+                  {isLogin ? 'Login' : 'Create Account'}
+                </button>
+                <button type='button' onClick={switchModeHandler}>
+                  {isLogin
+                    ? 'Create new account'
+                    : 'Login with existing account'}
+                </button>
+              </div>
+            </form>
+          )}
+        </div>
       )}
-    </div>
+    </Fragment>
   );
 };
 
