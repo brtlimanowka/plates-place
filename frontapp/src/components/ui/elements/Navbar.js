b78035acded7e4b4e375baaf51e3c3fe8cffcb83
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-10-05 15:26:57 +0200

    Navbar styling and functionality

--- frontapp/src/components/ui/elements/Navbar.js
+++ frontapp/src/components/ui/elements/Navbar.js
@@ -14,40 +14,44 @@ const Navbar = () => {
     <nav className={classes.navbar}>
       <header>
         <Link to='/'>
-          <i className='fas fa-dumbbell'></i>Plates' Place
+          <i className={`${classes.icon} fas fa-dumbbell`}></i>Plates' Place
         </Link>
       </header>
       {authContext.isAuthenticated && (
         <ul className={classes.menu}>
           <li className={classes['menu-item']}>
             <Link to='/planner'>
-              <i className={`${classes.icon} far fa-calendar-check`}></i>Plan My
-              Week
+              <i className={`${classes.icon} far fa-calendar-check`}></i>
+              <span className={classes['menu-item_label']}>Plan My Week</span>
             </Link>
           </li>
           <li className={classes['menu-item']}>
             <Link to='/workouts'>
-              <i className={`${classes.icon} fas fa-wrench`}></i>Configure
-              Workouts
+              <i className={`${classes.icon} fas fa-wrench`}></i>
+              <span className={classes['menu-item_label']}>Workouts</span>
             </Link>
           </li>
           <li className={classes['menu-item']}>
             <Link to='/stopwatch'>
-              <i className={`${classes.icon} fas fa-stopwatch`}></i>Stopwatch
+              <i className={`${classes.icon} fas fa-stopwatch`}></i>
+              <span className={classes['menu-item_label']}>Stopwatch</span>
             </Link>
           </li>
           <li className={classes['menu-item']}>
             <Link to='/settings'>
-              <i className={`${classes.icon} fas fa-sliders-h`}></i>Settings
+              <i className={`${classes.icon} fas fa-sliders-h`}></i>
+              <span className={classes['menu-item_label']}>Settings</span>
             </Link>
           </li>
           <li className={classes['menu-item']}>
             <Link to='/about'>
-              <i className={`${classes.icon} fas fa-ellipsis-h`}></i>About
+              <i className={`${classes.icon} fas fa-ellipsis-h`}></i>
+              <span className={classes['menu-item_label']}>About</span>
             </Link>
           </li>
           <li className={classes['menu-item']} onClick={logoutHandler}>
-            <i className={`${classes.icon} fas fa-sign-out-alt`}></i>Logout
+            <i className={`${classes.icon} fas fa-sign-out-alt`}></i>
+            <span className={classes['menu-item_label']}>Logout</span>
           </li>
         </ul>
       )}
