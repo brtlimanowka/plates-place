8a195ce5f298344b1e4441927d9193ccf69a88e7
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-10-06 15:23:13 +0200

    Set up handlebars

--- mailer/transporter.js
+++ mailer/transporter.js
@@ -1,4 +1,7 @@
 const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
+const path = require('path');
+
 const user = process.env.MAIL_USER;
 const pass = process.env.MAIL_PASSWORD;
 const clientId = process.env.MAIL_CLIENT_ID;
@@ -16,5 +19,15 @@ const transporter = nodemailer.createTransport({
     refreshToken,
   },
 });
+
+transporter.use(
+  'compile',
+  hbs({
+    viewEngine: {
+      defaultLayout: false,
+    },
+    viewPath: path.resolve(__dirname, 'templates'),
+  })
+);
 
 module.exports = transporter;
