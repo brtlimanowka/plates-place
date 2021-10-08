2c44a5c3f2250ebb06b7da156f70c683e22d2a82
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-10-08 12:52:36 +0200

    Handle password reset in frontapp

--- frontapp/src/components/pages/auth/ResetPassword.js
+++ frontapp/src/components/pages/auth/ResetPassword.js
@@ -1,13 +1,87 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import AuthContext from '../../../store/auth/authContext';
 import CenteredCard from '../../ui/templates/CenteredCard';
 import AuthInput from './AuthInput';
+import Spinner from '../../ui/elements/Spinner';
 import classes from '../css/Authentication.module.css';
 
 const ResetPassword = () => {
+  let history = useHistory();
+  const authContext = useContext(AuthContext);
+  const [formData, setFormData] = useState({
+    email: null,
+  });
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  useEffect(() => {
+    setIsFormValid(!!formData.email);
+  }, [formData]);
+
+  const emailConfirmed = (email) => {
+    setFormData({ email });
+  };
   const submitHandler = (event) => {
     event.preventDefault();
+    authContext.startLoading();
+    authContext.resetPassword();
   };
-  const emailConfirmed = () => {};
+  const backHandler = () => {
+    history.go(0);
+  };
+
+  const isContextLoading = authContext.isLoading;
+  const isContextError = !authContext.isLoading && !!authContext.error;
+  const isContextReset =
+    !authContext.isLoading && !!authContext.isPasswordReset;
+  const isContextEmptyForm =
+    !authContext.isLoading &&
+    !authContext.error &&
+    !authContext.isPasswordReset;
+
+  const renderLoading = <Spinner height='300px' />;
+  const renderError = (
+    <div className={classes['form-error']}>
+      <p>Application error: {authContext.error && authContext.error.message}</p>
+      <button type='button' onClick={backHandler}>
+        Back
+      </button>
+    </div>
+  );
+  const renderedReset = (
+    <div className={classes['form-success']}>
+      <p>Success!</p>
+      <p>
+        We've sent you an email with a link that'll allow you to reset your
+        password.
+      </p>
+      <button type='button' onClick={backHandler}>
+        Back
+      </button>
+    </div>
+  );
+  const renderEmptyForm = (
+    <form onSubmit={submitHandler}>
+      <h3>
+        We'll send you an email with a link where you'll be able to reset your
+        password
+      </h3>
+      <AuthInput
+        isLogin={true}
+        inputType='email'
+        inputFor='email'
+        inputLabel='Email'
+        confirmValue={emailConfirmed}
+      />
+      <div className={classes['form-actions']}>
+        <button
+          disabled={!isFormValid}
+          className={isFormValid ? '' : classes['form-invalid']}>
+          Send me the link
+        </button>
+      </div>
+    </form>
+  );
 
   return (
     <CenteredCard>
@@ -16,26 +90,10 @@ const ResetPassword = () => {
           <h1>Reset My Password</h1>
         </header>
         <div className={classes['form-container']}>
-          <form onSubmit={submitHandler}>
-            <h3>
-              We'll send you an email with a link where
-              <br /> you'll be able to reset your password
-            </h3>
-            <AuthInput
-              isLogin={true}
-              inputType='email'
-              inputFor='email'
-              inputLabel='Email'
-              confirmValue={emailConfirmed}
-            />
-            <div className={classes['form-actions']}>
-              <button
-                disabled={null}
-                className={false ? '' : classes['form-invalid']}>
-                Send me the link
-              </button>
-            </div>
-          </form>
+          {isContextLoading && renderLoading}
+          {isContextError && renderError}
+          {isContextReset && renderedReset}
+          {isContextEmptyForm && renderEmptyForm}
         </div>
       </main>
     </CenteredCard>
