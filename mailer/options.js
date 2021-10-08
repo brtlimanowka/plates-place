6e0dc9cea72bedf083634701de361a44c1bfdeba
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-10-08 18:47:19 +0200

    Improve Mailer's url builder

--- mailer/options.js
+++ mailer/options.js
@@ -1,13 +1,17 @@
 const setMailOptions = (template, host, recipient, manageString) => {
-  const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
-  const url = `${protocol}://${host}/api/users/${template}/${manageString}`;
+  const isProduction = process.env.NODE_ENV === 'production';
+  const protocol = isProduction ? 'https' : 'http';
+  let url = null;
   let subject;
 
   if (template === 'activate') {
+    url = `${protocol}://${host}/api/users/activate/${manageString}`;
     subject = "Activate your Plates' Place account";
   }
 
   if (template === 'reset') {
+    let urlHost = isProduction ? `https://${host}` : 'http://localhost:3000';
+    url = `${urlHost}/reset/${manageString}`;
     subject = "Plates' Place password reset request";
   }
 
