1aefba70e75284856496753cd975b86f64b59a4c
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-10-09 12:05:38 +0200

    Refactor Spinner into a styled component

--- frontapp/src/components/ui/elements/Spinner.js
+++ frontapp/src/components/ui/elements/Spinner.js
@@ -1,13 +1,46 @@
 import React from 'react';
-import CenteredCard from '../templates/CenteredCard';
-import classes from './css/Spinner.module.css';
+import styled from 'styled-components';
+import CenteredCard from '../../styles/CenteredCard.styled';
+
+const Dot = styled.div`
+  background-color: #f17104;
+  height: 20px;
+  width: 20px;
+  margin: 0 15px;
+  border-radius: 20px;
+  animation: pulse 1.5s linear infinite;
+  animation-delay: ${(props) => props.delay};
+
+  @keyframes pulse {
+    0% {
+      opacity: 1;
+      transform: scale(100%);
+    }
+    7% {
+      opacity: 0.3;
+      transform: scale(75%);
+    }
+    14% {
+      opacity: 0.1;
+      transform: scale(50%);
+    }
+    21% {
+      opacity: 0.3;
+      transform: scale(75%);
+    }
+    28% {
+      opacity: 1;
+      transform: scale(100%);
+    }
+  }
+`;
 
 const Spinner = (props) => {
   return (
     <CenteredCard height={props.height}>
-      <div id={classes.spinner_dot1} className={classes.spinner_dot}></div>
-      <div id={classes.spinner_dot2} className={classes.spinner_dot}></div>
-      <div id={classes.spinner_dot3} className={classes.spinner_dot}></div>
+      <Dot delay={'0s'} />
+      <Dot delay={'0.3s'} />
+      <Dot delay={'0.6s'} />
     </CenteredCard>
   );
 };
