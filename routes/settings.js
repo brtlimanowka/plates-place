117b5c969ab94103cc8c68c41efedd9fae5f70c7
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-10-16 16:44:23 +0200

    Set up PATCH for Settings

--- routes/settings.js
+++ routes/settings.js
@@ -4,14 +4,27 @@ const validators = require('./validators');
 const { validationResult } = require('express-validator');
 const Settings = require('../models/Settings');
 
-// @route   POST api/settings
-// @desc    Create settings for user
+// @route   PATCH api/settings
+// @desc    Update settings for user
 // @access  Private
-router.post('/', validators.settingsValidator, (req, res) => {
+router.patch('/:id', validators.settingsValidator, (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
+  let id = req.params.id;
+  Settings.findOneAndUpdate(
+    { user: id },
+    { bars: req.body.bars, weights: req.body.weights },
+    { new: true },
+    (error, updatedSettings) => {
+      if (error) {
+        return res.sendStatus(500);
+      } else {
+        return res.status(200).json(updatedSettings);
+      }
+    }
+  );
 });
 
 // @route   GET api/settings
