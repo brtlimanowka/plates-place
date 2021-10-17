465ed8243ff459c680f060700a05087d953182a5
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-10-17 16:34:52 +0200

    Click SettingsGroup Header to toggle contents

--- frontapp/src/components/pages/settings/SettingsGroup.js
+++ frontapp/src/components/pages/settings/SettingsGroup.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import SettingsContainer from '../../styles/SettingsContainer.styled';
 
+const ItemsContainer = styled.div`
+  display: ${(props) => (props.show ? 'block' : 'none')};
+  margin: 10px;
+  padding: 10px;
+  line-height: 24px;
+  li {
+    list-style: none;
+  }
+`;
+
 const SettingsGroup = (props) => {
+  const [showItems, setShowItems] = useState(false);
+
+  const headerClickHandler = () => setShowItems(!showItems);
+
   return (
     <SettingsContainer>
-      <header>{props.group}</header>
-      <div>
+      <header onClick={headerClickHandler}>{props.group}</header>
+      <ItemsContainer show={showItems}>
         <ul>
           <li>Item 1</li>
           <li>Item 2</li>
@@ -13,7 +28,7 @@ const SettingsGroup = (props) => {
           <li>Item 4</li>
           <li>Item 5</li>
         </ul>
-      </div>
+      </ItemsContainer>
     </SettingsContainer>
   );
 };
