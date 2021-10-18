1097358aee170f15218bfc195e6a2029e189f160
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-10-18 20:30:05 +0200

    Add button for new Bars/Weights

--- frontapp/src/components/pages/settings/SettingsGroup.js
+++ frontapp/src/components/pages/settings/SettingsGroup.js
@@ -8,14 +8,47 @@ const HeaderIcon = styled.i`
   margin: 0 10px 0 5px;
   color: ${(props) => props.theme.colors.buttonSecondaryBackground};
 `;
-
 const ItemsContainer = styled.div`
-  display: ${(props) => (props.show ? 'block' : 'none')};
+  display: ${(props) => (props.show ? 'flex' : 'none')};
+  flex-direction: column;
   margin: 10px;
   padding: 10px;
   line-height: 24px;
   li {
     list-style: none;
+  }
+`;
+const AddItemButton = styled.button`
+  background-color: ${(props) => props.theme.colors.backgroundLighter};
+  margin-top: 10px;
+  align-self: center;
+  width: 100%;
+  border: none;
+  border-radius: 3px;
+  text-transform: uppercase;
+  line-height: 24px;
+  letter-spacing: 0.8px;
+  font-weight: 700;
+  color: ${(props) => props.theme.colors.font};
+  &:hover {
+    cursor: pointer;
+    background-color: ${(props) =>
+      props.theme.colors.buttonSecondaryBackground};
+    color: ${(props) => props.theme.colors.buttonFont};
+  }
+
+  @media (min-width: 810px) {
+    width: 60%;
+  }
+`;
+const Icon = styled.i`
+  margin: 0 10px;
+  padding-top: 4px;
+  &:hover {
+    color: ${(props) =>
+      props.action === 'edit'
+        ? props.theme.colors.buttonPrimaryBackground
+        : props.theme.colors.errorBackground};
   }
 `;
 
@@ -37,6 +70,11 @@ const SettingsGroup = (props) => {
               <SettingsItem key={item._id} data={item} type={props.group} />
             ))}
         </ul>
+        {(props.group === 'Bars' || props.group === 'Weights') && (
+          <AddItemButton>
+            <Icon className='fas fa-plus'></Icon>Add New
+          </AddItemButton>
+        )}
       </ItemsContainer>
     </SettingsContainer>
   );
