0ec368ab2e50c98d646636db7d5f4137c747ca81
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-10-18 21:08:39 +0200

    Set up adding new Bar/Weight

---
+++ frontapp/src/components/pages/settings/SettingsNewItem.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from 'styled-components';
+import Button from '../../styles/Button';
+import ButtonIcon from '../../styles/ButtonIcon';
+
+const InputContainer = styled.div``;
+const Label = styled.label``;
+const TextInput = styled.input``;
+const NumberInput = styled.input``;
+const SelectInput = styled.select``;
+const ControlContainer = styled.div`
+  display: flex;
+`;
+const ControlButton = styled(Button)`
+  width: 50%;
+`;
+
+const SettingsNewItem = (props) => {
+  const thirdProperty =
+    props.type === 'Bars'
+      ? { for: 'type', label: 'Type' }
+      : { for: 'count', label: 'Count' };
+  const renderBarTypes = (
+    <SelectInput>
+      <option value='barbell'>Barbell</option>
+      <option value='dumbbell'>Dumbbell</option>
+      <option value='other'>Other</option>
+    </SelectInput>
+  );
+  const renderWeightsCount = <NumberInput type='number' min='1' step='1' />;
+
+  return (
+    <form>
+      <InputContainer>
+        <Label htmlFor='name'>Name:</Label>
+        <TextInput type='text' id='name' />
+        <Label htmlFor='weight'>Weight:</Label>
+        <NumberInput type='number' min='0' />
+        <Label htmlFor={thirdProperty.for}>{thirdProperty.label}</Label>
+        {props.type === 'Bars' ? renderBarTypes : renderWeightsCount}
+      </InputContainer>
+      <ControlContainer>
+        <ControlButton>
+          <ButtonIcon className='fas fa-check-circle'></ButtonIcon>Confirm
+        </ControlButton>
+        <ControlButton>
+          <ButtonIcon className='fas fa-times-circle'></ButtonIcon>Cancel
+        </ControlButton>
+      </ControlContainer>
+    </form>
+  );
+};
+
+export default SettingsNewItem;
