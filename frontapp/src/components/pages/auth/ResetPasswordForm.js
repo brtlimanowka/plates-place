e787fcff8b48576ac07d8c85a5a7646c42eb2256
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-10-28 18:55:02 +0200

    Remove password reset from Settings

--- frontapp/src/components/pages/auth/ResetPasswordForm.js
+++ frontapp/src/components/pages/auth/ResetPasswordForm.js
@@ -16,24 +16,22 @@ const ResetPasswordForm = ({ manageString }) => {
   const [isFormValid, setIsFormValid] = useState(false);
 
   useEffect(() => {
-    if (!authContext.isAuthenticated) {
-      setIsLoading(true);
-      fetch(`/api/auth/verify/${manageString}`, { cache: 'no-store' })
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.found) {
-            setFormData((prevFormData) => {
-              return { ...prevFormData, manageString };
-            });
-            setManageStringVerified({ confirmed: true });
-          } else {
-            setManageStringVerified({ confirmed: false });
-          }
-        })
-        .catch((error) => console.error(error))
-        .finally(() => setIsLoading(false));
-    }
-  }, [manageString, authContext.isAuthenticated]);
+    setIsLoading(true);
+    fetch(`/api/auth/verify/${manageString}`, { cache: 'no-store' })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.found) {
+          setFormData((prevFormData) => {
+            return { ...prevFormData, manageString };
+          });
+          setManageStringVerified({ confirmed: true });
+        } else {
+          setManageStringVerified({ confirmed: false });
+        }
+      })
+      .catch((error) => console.error(error))
+      .finally(() => setIsLoading(false));
+  }, [manageString]);
 
   useEffect(() => {
     setIsFormValid(!!formData.password && !!formData.manageString);
