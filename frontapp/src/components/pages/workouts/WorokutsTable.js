7612907e4b7819fdf9f4cdcc35d61ab7fd8ffb5f
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-10-30 15:18:03 +0200

    Add filtering to Workouts menu

--- frontapp/src/components/pages/workouts/WorokutsTable.js
+++ frontapp/src/components/pages/workouts/WorokutsTable.js
@@ -2,10 +2,52 @@ import React from 'react';
 import styled from 'styled-components';
 import SectionContainer from '../../styles/SectionContainer.styled';
 
+const TypeFilter = styled.div`
+  display: flex;
+`;
+const Types = styled.ul`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  margin-left: 10px;
+  li {
+    width: 50px;
+    margin: 0 5px;
+    padding: 2px 5px;
+    border-radius: 4px;
+    border: 1px solid ${(props) => props.theme.colors.background};
+    background-image: linear-gradient(
+      ${(props) => props.theme.colors.backgroundLighter},
+      ${(props) => props.theme.colors.background}
+    );
+    text-align: center;
+    font-size: 0.8rem;
+    text-transform: uppercase;
+    &:hover {
+      cursor: pointer;
+      background-image: linear-gradient(
+        ${(props) => props.theme.colors.background},
+        ${(props) => props.theme.colors.backgroundLighter}
+      );
+    }
+  }
+`;
+
 const WorokutsTable = () => {
   return (
     <SectionContainer>
-      <header>Table</header>
+      <header>
+        <TypeFilter>
+          Show:
+          <Types>
+            <li>All</li>
+            <li>Push</li>
+            <li>Pull</li>
+            <li>Legs</li>
+            <li>Other</li>
+          </Types>
+        </TypeFilter>
+      </header>
     </SectionContainer>
   );
 };
