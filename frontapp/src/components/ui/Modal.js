f7812322bb5e53ff704d17a34b4d49df1461202d
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-11-06 14:02:26 +0100

    Change modal hierarchy

--- frontapp/src/components/ui/Modal.js
+++ frontapp/src/components/ui/Modal.js
@@ -5,12 +5,17 @@ import styled from 'styled-components';
 const Backdrop = styled.div`
   position: fixed;
   display: flex;
+  align-items: center;
+  justify-content: center;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
   background-color: rgba(0, 0, 0, 0.6);
   z-index: 1000;
+  div#modal-container {
+    width: 60%;
+  }
 `;
 
 const Modal = (props) => {
@@ -26,7 +31,10 @@ const Modal = (props) => {
   }, []);
 
   const backdropClickHandler = (event) => {
-    if (event.target.parentElement.parentElement.id === 'modal-root') {
+    if (
+      event.target.parentElement.id === 'modal-root' ||
+      event.target.parentElement.id === 'modal-container'
+    ) {
       props.closeModal();
     }
   };
@@ -35,7 +43,9 @@ const Modal = (props) => {
     return null;
   } else {
     return createPortal(
-      <Backdrop onClick={backdropClickHandler}>{props.children}</Backdrop>,
+      <Backdrop onClick={backdropClickHandler}>
+        <div id='modal-container'>{props.children}</div>
+      </Backdrop>,
       document.getElementById('modal-root')
     );
   }
